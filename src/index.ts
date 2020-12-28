7184bc72aa61485899de7f02e9f28331fcc735c8
Calvin Colton <calvincolton@gmail.com>
2020-12-28 13:22:03 -0800

    completion of controllers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
-app.use(router);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['wargarble1999']}));
+app.use(AppRouter.getInstance());
 
 app.listen(4000, () => {
   console.log('Listening on port 4000');
